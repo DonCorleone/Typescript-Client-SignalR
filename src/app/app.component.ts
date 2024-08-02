82a3fa35ce81a384d2ce3789b212f9e6c5123afc
Linus Wieland <linus@wieland.click>
2024-08-02 11:53:47 +0200

    running client

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,30 @@
-import { Component } from '@angular/core';
+import {Component, OnInit, Signal} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SignalRService } from './services/signalr.service';
+import { FormsModule } from '@angular/forms';
+import {NgForOf} from "@angular/common";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule, NgForOf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'real-time-app';
+export class AppComponent implements OnInit{
+  user: string = '';
+  message: string = '';
+  messages!: Signal<string[]>; // Signal to store messages
+
+  constructor(private signalRService: SignalRService) {}
+  ngOnInit() {
+      this.signalRService.startConnection();
+      this.signalRService.addTransferChartDataListener();
+
+      this.messages = this.signalRService.getMessages(); // Use the signal from the service
+  }
+  sendMessage() {
+      this.signalRService.sendMessage(this.user, this.message);
+      this.message = '';
+  }
 }
---
+++ src/app/services/signalr.service.ts
@@ -0,0 +1,39 @@
+// src/app/services/signalr.service.ts
+import {Injectable, signal} from '@angular/core';
+import * as signalR from '@microsoft/signalr';
+@Injectable({
+  providedIn: 'root'
+})
+export class SignalRService {
+  private hubConnection: signalR.HubConnection;
+  private messages = signal<string[]>([]); // Signal to store messages
+
+  constructor() {
+    this.hubConnection = new signalR.HubConnectionBuilder()
+      .withUrl('http://localhost:5263/hub', {
+        withCredentials: true
+      })
+      .build();
+  }
+  startConnection(): void {
+    this.hubConnection
+      .start()
+      .then(() => console.log('Connection started'))
+      .catch(err => console.log('Error while starting connection: ' + err));
+  }
+  public addTransferChartDataListener() {
+    this.hubConnection.on('messageReceived', (user, message) => {
+      console.log(`User: ${user}, Message: ${message}`);
+      this.messages.update(messages => [...messages, message]);
+      // expose the message to the UI
+
+    });
+  }
+  getMessages() {
+    return this.messages.asReadonly(); // Expose messages as a readonly signal
+  }
+  public sendMessage(user: string, message: string) {
+    this.hubConnection.invoke('newMessage', user, message)
+      .catch(err => console.error(err));
+  }
+}
